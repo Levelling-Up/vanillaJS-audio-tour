91dd47ddfe2481d586e1e741799b1004b4a8a942
ecalogero <ecalogero@gmail.com>
2021-05-07 18:39:47 +0100

    attempted to switch to https

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
+var http = require('http');
+var enforce = require('express-sslify');
+
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8000;
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+
+// Because we are behind a load balancer in Heroku, we use {trustProtoHeader: true}
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
